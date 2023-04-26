34c4166c34f6ad47b04f7ddb25955ae901a4418b
centellesGuillem <centellesg@aia.es>
2023-04-26 19:29:47 +0200

    Implemented check_basecase_dir function

--- contingencies-screening/src/dynawo_contingencies_screening/prepare_basecase/prepare_basecase.py
+++ contingencies-screening/src/dynawo_contingencies_screening/prepare_basecase/prepare_basecase.py
@@ -1,5 +1,25 @@
-def check_basecase_dir(basecase_dir):
+from pathlib import Path, PurePath
+
+
+def check_basecase_dir(input_dir):
     # Check input has both directories
-    # Check hades dir has only 1 file (.xml)
+    if not Path(input_dir / "dynawo").is_dir():
+        print("Error: missing 'dynawo' directory in the input folder (name must be as shown in this message)")
+        exit()
+    if not Path(input_dir / "hades").is_dir():
+        print("Error: missing 'hades' directory in the input folder (name must be as shown in this message)")
+        exit()
+
+    # Check hades dir is not empty and has only 1 file ('donneesEntreeHADES2*.xml')
+    has_next = next(Path(input_dir / "hades").iterdir(), None)
+    if has_next is not None:
+        for file in Path(input_dir / "hades").iterdir():
+            if not (PurePath(file).name.startswith("donneesEntreeHADES2")) or (file.suffix != ".xml"):
+                print("Error: 'hades' directory in the input folder "
+                      "should only contain the 'donneesEntreeHADES2*.xml' case-file")
+                exit()
+    else:
+        print("Error: 'hades' directory in the input folder should not be empty")
+        exit()
+
     # TODO: Check dynawo directory
-    pass
